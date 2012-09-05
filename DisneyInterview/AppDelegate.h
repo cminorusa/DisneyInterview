3ffbe535c4e7f277ed2b12797b43dc9d09843420
cminorusa@gmail.com <thinkcy2k>
2012-09-05 10:05:07 -0700

    Initial Commit

---
+++ DisneyInterview/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DisneyInterview
+//
+//  Created by Clifford Sharp on 9/5/12.
+//  Copyright (c) 2012 Clifford Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DisneyInterview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DisneyInterview
+//
+//  Created by Clifford Sharp on 9/5/12.
+//  Copyright (c) 2012 Clifford Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
